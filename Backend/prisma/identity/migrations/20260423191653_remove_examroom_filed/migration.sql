5c78cde5a18df7205132cab71425cb0361197baf
MOHAMEDbelghait <belghaitmohamd1@gmail.com>
2026-05-21 15:20:40 +0100

    fixing the migration issues

--- Backend/prisma/identity/migrations/20260423191653_remove_examroom_filed/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `examRoom` on the `competition_sessions` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "competition_sessions" DROP COLUMN "examRoom";
